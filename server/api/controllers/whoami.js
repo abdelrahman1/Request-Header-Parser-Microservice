8b927bbd9bc9a48606fcf1657a7f543513e4d536
Abdelrahman <abdelrahman.ashraf.mohamed@gmail.com>
2018-03-25 09:22:23 +0200

    update ipaddress

--- server/api/controllers/whoami.js
+++ server/api/controllers/whoami.js
@@ -1,7 +1,7 @@
 let whoAmIcontroller = {
   index: async (req, res, next) => {
     res.status(200).json({
-      ipaddress:req.ip,
+      ipaddress:req.headers['x-forwarded-for'] || req.connection.remoteAddress,
       language:req.headers['accept-language'],
       software:req.headers['user-agent']
     })
